99322f3f5dac66ce4d7294c8cff5480a0902947e
vtiron2002 <vtiron2002@gmail.com>
2020-05-06 21:43:42 -0400

    Updates

---
+++ precache-manifest.16f7fad2b4d44d71f011eb1b8775415e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29dbef60c79a444a6edfe7fad7c7fd38",
+    "url": "/noteman/index.html"
+  },
+  {
+    "revision": "534b45ac8958a271c9d0",
+    "url": "/noteman/static/css/main.ee92e3cd.chunk.css"
+  },
+  {
+    "revision": "9e115349ecff16dea822",
+    "url": "/noteman/static/js/2.d7991a9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/noteman/static/js/2.d7991a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "534b45ac8958a271c9d0",
+    "url": "/noteman/static/js/main.c8afb68a.chunk.js"
+  },
+  {
+    "revision": "24329451b028410b343c",
+    "url": "/noteman/static/js/runtime-main.7ff7e177.js"
+  }
+]);
